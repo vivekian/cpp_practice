09158861f9da9c8e6c09364147875b61fed40111
Vivek Aseeja <vaseeja@uh.edu>
2015-10-14 18:09:21 -0700

    fix tabs and spaces

--- effective_cpp/item45/main.cpp
+++ effective_cpp/item45/main.cpp
@@ -6,31 +6,31 @@ using namespace std;
 class A 
 { 
 public: 
-	A() {}
-	~A() {}
+    A() {}
+    ~A() {}
 
-	A(const A &rhs)
-	{ 
-		cout << "copy constructor" << endl;
-		this->x = rhs.x;
-	}
+    A(const A &rhs)
+    { 
+        cout << "copy constructor" << endl;
+        this->x = rhs.x;
+    }
 
-	A& operator=(const A &rhs)
-	{ 
-		cout << "assignment operator" << endl; 
-		this->x = rhs.x;
-		return *this;
-	}
+    A& operator=(const A &rhs)
+    { 
+        cout << "assignment operator" << endl; 
+        this->x = rhs.x;
+        return *this;
+    }
 
 private: 
-	int x;
+    int x;
 };
 
 int main() 
 { 
-	A a;
-	A b(a);
-	A c;
+    A a;
+    A b(a);
+    A c;
     c = b;
-	return 0;
+    return 0;
 } 
