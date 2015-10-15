d93994da9452b7c840c1541e43873af6ef14965c
Vivek Aseeja <vivekian@Viveks-MacBook-Pro.local>
2015-10-14 18:00:33 -0700

    copy ctor and assignment operator impl.

---
+++ effective_cpp/item45/main.cpp
@@ -0,0 +1,36 @@
+// copy constructor and assignment operator example 
+
+#include <iostream> 
+using namespace std; 
+
+class A 
+{ 
+	public: 
+		A() {}
+		~A() {}
+
+		A(const A &rhs)
+		{ 
+			cout << "copy constructor" << endl;
+			this->x = rhs.x;
+		}
+
+		A& operator=(const A &rhs)
+		{ 
+			cout << "assignment operator" << endl; 
+			this->x = rhs.x;
+			return *this;
+		}
+
+	private: 
+		int x;
+};
+
+int main() 
+{ 
+	A a;
+	A b(a);
+	A c;
+    c = b;
+	return 0;
+} 
