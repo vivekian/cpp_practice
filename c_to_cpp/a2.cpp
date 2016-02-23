27d68d1037d0e1df8a607ac38a7a667f8f106062
vivekian <vivekaseeja@me.com>
2016-02-22 17:36:18 -0800

    finalize graph as a class implementation

--- c_to_cpp/a2.cpp
+++ c_to_cpp/a2.cpp
@@ -1,47 +1,60 @@
-//rudimentary graph representation and BFS solution. 
+// Graph class representation
 
 #include <algorithm> 
 #include <cstdio> 
 #include <queue>
 #include <stdint.h> 
+#include <iostream> 
 
+using namespace std; 
+
+// Maximum number of nodes in a graph
 namespace { 
     const int MAXV = 100; 
 }
 
-
-// edgeNode holds the information about each edge for each edge
-class edgeNode 
+// class - EdgeNode
+// purpsose - data structure to hold edge data from one node to another node
+//            including weight. also has a pointer to the next node in the edgelist
+class EdgeNode 
 {
     public: 
-        edgeNode(const uint32_t i, const uint32_t w):
+        EdgeNode(const uint32_t i, const uint32_t w):
             idx(i), weight(w), next(nullptr)
         {}
 
         uint32_t idx;       // index of the node in the graph
         uint32_t weight;    // weight of the edge 
-        edgeNode* next;     // pointer to the next node in the list
+        EdgeNode* next;     // pointer to the next node in the list
 };
 
-
+// class - Graph 
+//
+// implementation of a edgelist form for the graph. 
+// e.g. consider the graph below with 4 vertices
+//      0 -> 1 -> 3 
+//      1 -> 2 -> 0 
+//      2 -> 1 
+//      3 -> 1 
 class Graph
 { 
     public: 
         // constructor to setup the graph. 
         // expects user to input the total number of vertices in the graph 
-        Graph(const uint32_t num_vertices): nvertices(num_vertices), edges(0)
+        Graph(const uint32_t num_vertices): 
+            nvertices(num_vertices), edges(0)
         {
             Initialize(); 
         }
 
         // destructor - empty as there is no dynamically allocated memory
-        ~Graph();
+        ~Graph(){}
 
         // setup the nodelist and the outward degree value for each node to 0. 
         void Initialize() 
         {
-            std::fill(nodelist, nodelist+MAXV, nullptr); // initialize empty lists
-            std::fill(degree, degree+MAXV, 0);           // initialize the degree to 0 for each node
+            fill(nodelist, nodelist+MAXV, nullptr); // initialize empty nodelist
+            fill(degree, degree+MAXV, 0);           // initialize the degree to 0 for each node
         }
 
         int V() { return nvertices; }       // return total number of vertices in the graph
@@ -51,7 +64,7 @@ class Graph
         bool adjacent(const uint32_t x, const uint32_t y) 
         { 
             bool ret = false;
-            edgeNode* p = nodelist[x]; 
+            EdgeNode* p = nodelist[x]; 
             
             // iterate through all nodes to find a match
             while (p) { 
@@ -69,7 +82,7 @@ class Graph
         // this requires to also create a node from y to x
         void add(const uint32_t x, const uint32_t y, bool IsReverse)
         { 
-            edgeNode* tmp = new edgeNode(y, 0);         // create a new empty node 
+            EdgeNode* tmp = new EdgeNode(y, 0);         // create a new empty node 
             tmp->next = nodelist[x];                    // insert at the beginning of list
             nodelist[x] = tmp;                          // append the rest of the list 
             degree[x]++;                                // increment the edge degree of node
@@ -77,117 +90,102 @@ class Graph
             // create an edge from y to x as well
             if (!IsReverse) { 
                 edges++;                                    // increment total edges
-                addEdge(y, x, true); 
+                add(y, x, true); 
             }
         } 
+       
+        // delete an edge from the graph  
+        void del(const uint32_t x, const uint32_t y, bool IsReverse) 
+        {
+            // iterate with two pointers till we find the node 
+            // once found, then have the previous node's next point to the 
+            // next node.  
+            EdgeNode* curr = nodelist[x]; 
+            EdgeNode* prev = nodelist[x]; 
+            
+            while(curr) { 
+                if (curr->idx == y) { 
+                    prev->next = curr->next; 
+                    delete curr; 
+                    break; 
+                }
+                prev = curr; 
+                curr=curr->next; 
+            }
+
+            // delete the reverse edge as well from y to x
+            if (!IsReverse) { 
+                del(y, x, true); 
+            }
+        }
 
         void set_edge_value(const uint32_t x, const uint32_t y, const uint32_t v) 
+        {
+           EdgeNode* p = nodelist[x]; 
+          
+           // iterate to find the edge from x to y and set the value(weight) correctly 
+           while(p) { 
+              if (p->idx == y) { 
+                 p->weight = v; 
+                 break; 
+              }
+
+              p = p->next; 
+           }
+        }
+
+        uint32_t get_edge_value(const uint32_t x, const uint32_t y) 
+        {
+           EdgeNode* p = nodelist[x]; 
+           uint32_t w = 0; 
+
+           while(p) { 
+              if (p->idx == y) { 
+                w = p->weight; 
+              }
+              p = p->next; 
+           } 
+
+           return w; 
+        }
+
+        void print() 
         { 
-        } 
-            
+           for (int i=0; i<nvertices; ++i) { 
+                cout << i << " -> ";  
+                EdgeNode* p = nodelist[i]; 
+
+                while(p) {
+                    cout << p->idx << " -> "; 
+                    p = p->next; 
+                }
+
+                cout << endl; 
+           }
+        }               
+     
     private:
-        edgeNode* nodelist[MAXV];   // adjacency list with edge information about each node  
+        EdgeNode* nodelist[MAXV];   // adjacency list with edge information about each node  
         uint32_t degree[MAXV];      // degree of each node in the graph 
         uint32_t nvertices;         // total number of vertices currently on the graph 
         uint32_t edges;             // total number of edges in the graph
 };
 
 
-typedef struct edgeNode { 
-    uint32_t y; 
-    edgeNode* next; 
-} edgeNode;
-
-typedef struct graph { 
-    edgeNode* nodelist[MAXV]; 
-    uint32_t degree[MAXV]; 
-    uint32_t nvertices; 
-} graph; 
-
-void Initialize(graph* g) 
-{ 
-    std::fill(g->nodelist, g->nodelist + MAXV, nullptr); 
-    std::fill(g->degree, g->degree + MAXV, 0); 
-    g->nvertices = 0; 
-}
-
-// watch out for the recursive call to make sure there is an edge 
-// from y to x as well. IsReverse just links on the other side.
-void InsertEdge(graph* g, const int x, const int y, const bool IsReverse)
-{
-    edgeNode* node = new edgeNode; 
-    node->y = y; 
-    node->next = g->nodelist[x]; 
-    g->nodelist[x] = node; 
-    g->degree[x]++; 
-
-    if (!IsReverse)
-        InsertEdge(g, y, x, true);  
-}
-
-void PrintGraph(const graph* const g) 
-{ 
-    for (int i=0; i<g->nvertices; ++i) { 
-        printf("%d -> ", i); 
-        edgeNode* p = g->nodelist[i]; 
-        while (p) { 
-            printf("%d -> ", p->y);
-            p = p->next;
-        }
-        printf("\n"); 
-    }
-}
-
-void BFS(const graph* g, uint32_t d[])
-{
-    std::queue<int> q;
-    
-    for (int i=0; i<g->nvertices; ++i) 
-        d[i] = UINT32_MAX; 
-
-    q.push(0); 
-    d[0] = 0; 
-
-    while (!q.empty()) { 
-        
-        int node = q.front();
-        q.pop();
-        edgeNode* p = g->nodelist[node]; 
-        
-        while(p) {
-          if (d[p->y] == UINT32_MAX) { 
-                d[p->y] = d[node] + 1; 
-                q.push(p->y); 
-          } 
-          p=p->next; 
-        }
-
-    }
-
-    return; 
-}
-
 int main() 
 { 
-    graph g; 
-    Initialize(&g);
-    g.nvertices = 6; 
+    Graph g(6); 
 
-    InsertEdge(&g, 0, 1, false);
-    InsertEdge(&g, 0, 5, false);
-    InsertEdge(&g, 1, 2, false);   
-    InsertEdge(&g, 1, 4, false);  
-    InsertEdge(&g, 2, 3, false);  
-    InsertEdge(&g, 3, 4, false);  
-    InsertEdge(&g, 4, 5, false);  
-    
-    PrintGraph(&g); 
+    g.add(0, 1, false);
+    g.add(0, 5, false);
+    g.add(1, 2, false);   
+    g.add(1, 4, false);  
+    g.add(2, 3, false);  
+    g.add(3, 4, false);  
+    g.add(4, 5, false);  
+    g.print(); 
 
-    uint32_t d[MAXV]; 
-    BFS(&g, d); 
-
-    for(int i=0; i<g.nvertices; ++i) 
-        printf("[%d]: %u\n", i, d[i]); 
+    cout << g.E() << endl; 
 
     return 0; 
 }
