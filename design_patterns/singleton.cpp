4c6e7ab817df6b1a32576eba359c7f2a7a02c2b0
vivekian <vivekaseeja@me.com>
2017-01-16 08:09:18 -0800

    Implementation of Myers singleton

---
+++ design_patterns/singleton.cpp
@@ -0,0 +1,39 @@
+#include <atomic> 
+#include <iostream> 
+#include <thread>
+
+class Singleton 
+{ 
+    public: 
+    static Singleton& Instance() 
+    { 
+        static Singleton s; 
+        return s; 
+    }
+   
+    private: 
+    Singleton() { std::cout << "Initialized" << std::endl; } 
+    ~Singleton() = default; 
+
+    Singleton(const Singleton&) = delete; 
+    Singleton& operator=(const Singleton&) = delete; 
+}; 
+
+void foo()
+{ 
+    Singleton& s = Singleton::Instance();
+} 
+
+int main() 
+{ 
+    std::thread first(foo); 
+    std::thread second(foo); 
+    std::thread third(foo); 
+
+    first.join(); 
+    second.join(); 
+    third.join(); 
+
+    Singleton &s = Singleton::Instance(); 
+} 
+
